60b722cfc776f6e9f5d6dc2c5244575aab799ec6
Miguel <miguellukas52@gmail.com>
2020-08-24 12:25:58 -0300

    update readme.md

--- lab-25.08.2020/exerc12.c
+++ lab-25.08.2020/exerc12.c
@@ -1,10 +1,13 @@
 #include "stdio.h"
+#include "windows.h"
 
 int main(void)
 {
+    SetConsoleOutputCP(65001);
+
     float factory_price, consumer_price;
 
-    printf("Digite o valor de fabrica: ");
+    printf("Digite o valor de fábrica: ");
     scanf("%f", &factory_price);
 
     consumer_price = factory_price + ((28.0 / 100.0) * factory_price) + ((45.0 / 100.0) * factory_price);
