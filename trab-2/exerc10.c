32346cc00e332c375180ca33eace7757a17af532
Miguel <miguellukas52@gmail.com>
2020-09-10 14:46:28 -0300

    trab 2

---
+++ trab-2/exerc10.c
@@ -0,0 +1,70 @@
+/*
+
+*  **
+*  * Created by Miguel on 10/09/2020.
+*  **
+
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include "stdlib.h"
+
+float distance(float x1, float y1, float x2, float y2)
+{
+    return sqrt(pow(x2 - x1, 2.0) + pow(y2 - y1, 2.0));
+}
+
+int main(void)
+{
+    float x1, y1, x2, y2;
+
+    char line[100];
+    char split[] = ",";
+
+    size_t counter = 0;
+
+    while (1)
+    {
+        printf("Digite as coordenadas da seguinte forma: (X1,Y1,X2,Y2): ");
+        scanf("%s", &line);
+
+        char *ptr = strtok(line, split);
+
+        while (ptr != NULL)
+        {
+            if (counter == 0)
+            {
+                x1 = atof(ptr);
+            }
+
+            if (counter == 1)
+            {
+                y1 = atof(ptr);
+            }
+
+            if (counter == 2)
+            {
+                x2 = atof(ptr);
+            }
+
+            if (counter == 3)
+            {
+                y2 = atof(ptr);
+            }
+
+            ptr = strtok(NULL, split);
+
+            counter++;
+        }
+
+        counter = 0;
+
+        printf("\nDistance: %f\n\n", distance(x1, y1, x2, y2));
+
+        if (x1 == 0 && x2 == 0 && y1 == 0 && y2 == 0)
+            break;
+    }
+
+    return 0;
+}
