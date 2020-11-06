50afbf614f4b4f88295cb4f14504d0734c435a3f
Miguel <miguellrk16@gmail.com>
2020-11-06 16:02:00 -0300

    .

--- revisao/lab/exerc1/exerc1.c
+++ revisao/lab/exerc1/exerc1.c
@@ -1,43 +1,38 @@
 #include <stdio.h>
 
-int main(void)
-{
-  int m[4][6], n[6][4];
+int main(void) {
+    int m[4][6], n[6][4];
 
-  for (size_t i = 0; i < 4; ++i) 
-  {
-    for (size_t j = 0; j < 6; ++j)
-    {
-      printf("\nDigite o valor do elemento M(%d, %d): ", i + 1, j + 1);
-      scanf("%d", &m[i][j]);
+    for (int i = 0; i < 4; ++i) {
+        for (int j = 0; j < 6; ++j) {
+            printf("\nDigite o valor do elemento M(%d, %d): ", i + 1, j + 1);
+            scanf("%d", &m[i][j]);
+        }
     }
-  }
 
-  for (size_t i = 0; i < 6; ++i) 
-  {
-    for (size_t j = 0; j < 4; ++j)
-    {
-      printf("\nDigite o valor do elemento N(%d, %d): ", i + 1, j + 1);
-      scanf("%d", &n[i][j]);
+    for (int i = 0; i < 6; ++i) {
+        for (int j = 0; j < 4; ++j) {
+            printf("\nDigite o valor do elemento N(%d, %d): ", i + 1, j + 1);
+            scanf("%d", &n[i][j]);
+        }
     }
-  }
 
-  int sumLineColumn[4];
+    int sums[4] = {0};
 
-  for (size_t i = 0; i < 4; ++i) 
-  {
-    sumLineColumn[i] = 0;
+    int aux = 0;
 
-    for (size_t j = 0; j < 6; ++j)
-    {
-      sumLineColumn[i] += m[i][j] + n[j][i];
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++) {
+            aux += m[i][j] + n[j][i];
+        }
+
+        sums[i] = aux;
+        aux = 0;
     }
-  }
 
-  for (size_t i = 0; i < 4; ++i)
-  {
-    printf("\nSoma da %d linha com a %d coluna: %d", i + 1, i + 1, sumLineColumn[i]);
-  }
+    for (int k = 0; k < 4; k++) {
+        printf("\nSoma %d: %d", (k + 1), sums[k]);
+    }
 
-  return 0;
+    return 0;
 }
